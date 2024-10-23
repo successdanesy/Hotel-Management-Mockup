e87e66f70f916047148db661a3d3762a12e8f31c
Successdanesy <successdanesy@gmail.com>
2024-10-23 23:44:17 +0100

    multiple additions, majorly the reports page

---
+++ reports.js
@@ -0,0 +1,8 @@
+// Adding basic functionality for buttons
+document.querySelector('.export-btn').addEventListener('click', () => {
+    alert('Report Exported');
+});
+
+document.querySelector('.generate-btn').addEventListener('click', () => {
+    alert('Generating New Report...');
+});
